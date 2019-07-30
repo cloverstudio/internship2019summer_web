3b4b0de2ccec98371cab469864474289264a7321
chloe0592 <klaudija.ljevar@gmail.com>
2019-07-30 13:50:21 +0200

    added bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import Routes from './Routes';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render (){
