1f0288904ba54490569cb7dd136b242c98894f19
chloe0592 <klaudija.ljevar@gmail.com>
2019-08-05 13:59:01 +0200

    Adding styling to middle screen

--- frontend/src/components/MiddleScreen.js
+++ frontend/src/components/MiddleScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from './layout/Header';
+import Footer from './layout/Footer';
 import icon_checkmark from '../assets/log_in_checkmark_icon.svg';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 
 class MiddleScreen extends Component {
+
     render() {
       return (
         <Router>
@@ -17,18 +19,18 @@ class MiddleScreen extends Component {
                   Čestitamo, uspješno ste <br></br> 
                   se prijavili na sustav MojGrad!
                 </p>
-                <p style={{marginTop: '15px', marginBottom: '50px'}}>
+                <p style={{marginTop: '15px', marginBottom: '35px'}}>
                   Kako bi vam omogućili najbolje iskustvo <br></br> 
                   korištenja aplikacije, spajamo vaše <br></br> 
                   podatke s aplikacijom.
                 </p>
                 <img style= {{maxWidth: "200px"}} alt= "checkmark icon" src = {icon_checkmark}/>
-                <p style={{marginTop: '50px'}}>
+                <p style={{marginTop: '35px'}}>
                   3 sec...
                 </p>
-                
               </div>
             </div>
+            <Footer style={{textAlign: 'center'}}/>
           </div>
         </Router>
         )
--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 function Footer() {
-        return <p style={{fontSize: '14px', marginBottom: '0px'}}>
+        return <p style={{fontSize: '14px', marginBottom: '0px', marginTop: '15PX' ,textAlign: 'center'}}>
           Aplikacija je namjenjena građanima kako bi mogli prijaviti kvarove, <br>
           </br>izraziti svoje mišljenje i informirati se o aktualnim temama.
         </p>;
