e6e5532a830dc709add2558c48a9d8220764f8d7
Antonio <srecicantonio@gmail.com>
2019-08-08 10:28:30 +0200

    updated knex config
    updated knex config

--- backend/src/server/config/development.js
+++ backend/src/server/config/development.js
@@ -1,8 +1,11 @@
-const db_host =  'localhost';
-const db_user = 'root';
-const db_password = '!@Antonio22';
-const db_name = 'MojGrad';
+const knexConfig = require('./knexfile');
+
+const db_host =  knexConfig.locdevelopmental.connection.host;
+const db_user = knexConfig.development.connection.user;
+const db_password = knexConfig.development.connection.password;
+const db_name = knexConfig.development.connection.database;
 JWT_SECRET='jsonwebtokensecretwordabc';
+PORT = 8080;
 
 module.exports = {
     database: {
