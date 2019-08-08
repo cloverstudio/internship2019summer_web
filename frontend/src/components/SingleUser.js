450acda150b12900c87a8b0c6858049b0f3770ef
DevilDeceiver <46030196+DevilDeceiver@users.noreply.github.com>
2019-08-08 10:06:24 +0200

    added fonts

--- frontend/src/components/SingleUser.js
+++ frontend/src/components/SingleUser.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import nav_users_icon from '../assets/nav_users_icon.svg';
 
 const SingleUser = ({name, email, img}) =>{
         return (
-            <div>
-                <img src={img} alt=""/>
-                <div>
+            <div className="user">
+                <img className="profile-photo" src={nav_users_icon} alt=""/>
+                <div className="user-info">
                     <h2>{name}</h2>
                     <p>{email}</p>
                 </div>
