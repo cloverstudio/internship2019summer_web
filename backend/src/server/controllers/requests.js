27c3efa9c529ae122249dcb48819868785ff888a
Antonio <srecicantonio@gmail.com>
2019-08-16 17:39:24 +0200

    fixed new token api
    fixed new token api

--- backend/src/server/controllers/requests.js
+++ backend/src/server/controllers/requests.js
@@ -3,13 +3,22 @@ const router = express.Router();
 const dbFunction = require('../lib/dbFunctions');
 const jwt_decode = require('jwt-decode');
 const tokenFunction = require('../lib/tokenFunctions');
+const consts = require('../lib/consts');
 
 router.post('/new', async (req, res) => {
     let data = req.body;
 
-    let isLoggedIn = await tokenFunction.checkTokenAvailability(req.headers.authorization.split(" ")[1]);
-    let userId = await dbFunction.findUserID(jwt_decode(req.headers.authorization.split(" ")[1]).email);
+    let isLoggedIn = await tokenFunction.checkTokenAvailability(req.headers.token);
+    let userId = await dbFunction.findUserID(jwt_decode(req.headers.token).email);
 
+    if(!isLoggedIn) {
+        res.status(440).json({ 'data': {
+            'error': {
+                'error_code': consts.responseErrorExpiredToken.error_code,
+                'error_description': consts.responseErrorExpiredToken.error_description
+            }
+        }})
+    }
     dbFunction.newRequest(data.title, data.Request_type, data.location_latitude, data.location_longitude, data.message, userId);
 
     res.status(200).json('ok!');
