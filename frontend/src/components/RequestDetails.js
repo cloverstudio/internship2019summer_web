d55de571686a1437213bd74c9a3cd64303ca59a6
chloe0592 <klaudija.ljevar@gmail.com>
2019-08-28 10:27:39 +0200

    made filter for all requests

--- frontend/src/components/RequestDetails.js
+++ frontend/src/components/RequestDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
-import {CardDeck, Card} from 'react-bootstrap';
+import {CardDeck, Card, Button} from 'react-bootstrap';
 import SideBar from './layout/SideBar';
 
 export class RequestDetails extends Component {
@@ -19,7 +19,63 @@ export class RequestDetails extends Component {
       <div style={{display:'flex'}}>
         <SideBar />
         <div className="requests-container-gray">
-          <h1>Details</h1>
+         <Button
+            className=""
+            color="primary"
+            size="sm"
+            href="/Requests">
+            Vrati se nazad
+         </Button>{' '}
+
+          <h1>Detalji</h1>
+          
+          <form>
+              <label htmlFor="name">Naslov:</label>
+              <div className="input-field">
+                <input type="text" name="title" ref="title" />
+              </div>
+
+              <label htmlFor="type">Tip zahtjeva:</label>
+              <div className="input-field">
+                <select ref="Request_type">
+                  <option value="kvar" name="kvar">Kvar</option>
+                  <option value="prijedlog" name="prijedlog">Prijedlog</option>
+                </select>
+              </div>
+
+              <label htmlFor="photo" action="intern2019.def.clover.studio/requests/new" method="POST" encType="multipart/form-data">Datoteka:</label>
+              <div className="input-field">
+                <input type="file" name="photo" id="exampleFile" ref="photo" />
+              </div>
+
+              <label htmlFor="address">Upišite lokaciju:</label>
+              <div className="input-field">
+                <input type="text" name="address" ref="address" />
+              </div>
+
+              <label htmlFor="city">Poruka:</label>
+              <div className="input-field">
+                <input type="text" name="message" ref="message" />
+              </div>
+
+              <div className="two-btns-request">
+                <Button
+                  color="primary"
+                  type="submit"
+                  // onClick={this.onSubmit.bind(this)}
+                  >
+                  Pošalji zahtjev
+                </Button>{' '}
+
+                <Button
+                  className="outd-btn"
+                  color="primary"
+                  href="/Requests">
+                  Poništi zahtjev
+                </Button>{' '}
+
+              </div>
+            </form>
         </div>
         
       </div>
--- frontend/src/components/Requests.js
+++ frontend/src/components/Requests.js
@@ -2,78 +2,108 @@ import React, { Component } from 'react'
 import RequestItem from './RequestItem';
 import add_icon from '../assets/add_icon.svg';
 import SideBar from './layout/SideBar';
-import {CardDeck, Card} from 'react-bootstrap'
+import { CardDeck, Card } from 'react-bootstrap'
+import { throwStatement } from '@babel/types';
 
 export class Requests extends Component {
-  constructor(){
+  constructor() {
     super();
+
+    this.requests=[];
     this.state = {
-      requests: []
+      filteredRequests: []
     }
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.getRequests();
   }
 
 
-  async getRequests(){
+  async getRequests() {
     console.log(localStorage.getItem('token'));
     try {
-      
-      await fetch('https://intern2019dev.clover.studio/requests/myRequests', 
-      //admin 'https://intern2019dev.clover.studio/requests/all'
-      {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'token': localStorage.getItem('token')
-        },
-        method: 'GET'
-      }).then(async (response) =>{
-        const json = await response.json();
-        console.log(json.data.requests);
-        this.setState({
-          requests: json.data.requests
-        });
-    })
+
+      await fetch('https://intern2019dev.clover.studio/requests/myRequests',
+        //admin 'https://intern2019dev.clover.studio/requests/all'
+        {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'token': localStorage.getItem('token')
+          },
+          method: 'GET'
+        }).then(async (response) => {
+          const json = await response.json();
+          console.log(json.data.requests);
+
+          this.requests=json.data.requests;
+          this.setState({
+            filteredRequests: json.data.requests
+          });
+        })
     } catch (err) {
       console.log(err)
     }
-   
   }
 
+  filterRequests = () => {
+    let wantedType = this.refs.Request_type.value;
 
-    render() {
-      const RequestItems = this.state.requests.map((request, i) => {
-        console.log(request);
-        return(
-          <RequestItem key={request.ID} item={request} />
-        )
-      })
+    let allRequests = this.requests;
+    var filteredRequests = allRequests.filter(request => {
 
+      if (wantedType !== 'sve')
+        return request.Request_type === wantedType
+
+      return request;
+
+    });
+    this.setState({
+      filteredRequests: filteredRequests
+    });
+  }
+
+  render() {
+    const RequestItems = this.state.filteredRequests.map((request, i) => {
       return (
-        <div style={{display:'flex'}}>
-           <SideBar />
-            <div className="requests-container-gray">
-              <div className="new-requests-container">
-                <Card style={{border: 'none'}}>
-                <a className="requests-icon" href="/NewRequest"> 
-                  <img style= {{maxWidth: "200px"}} src = {add_icon} alt="Add more"/>
-                    <div className="requests-text">
-                      <p>Novi zahtjev</p>
-                    </div>
-                </a>
-                </Card>
-                
-              </div>
-                <ul className="collection" style={{listStyleType: 'none'}}>
-                  {RequestItems}
-                </ul>     
+        <RequestItem key={request.ID} item={request} />
+      )
+    })
+
+    return (
+      <div style={{ display: 'flex' }}>
+        <SideBar />
+        <div className="requests-container-gray">
+
+          <div className="filter-type">
+            <div className="input-field">
+              <select ref="Request_type" onChange={this.filterRequests.bind(this)}>
+                <option value="sve" name="sve">Sve</option>
+                <option value="kvar" name="kvar">Kvar</option>
+                <option value="prijedlog" name="prijedlog">Prijedlog</option>
+              </select>
             </div>
-         </div>
-        )
-    }
+          </div>
+
+          <div className="new-requests-container">
+            <Card style={{ border: 'none' }}>
+              <a className="requests-icon" href="/NewRequest">
+                <img style={{ maxWidth: "200px" }} src={add_icon} alt="Add more" />
+                <div className="requests-text">
+                  <p>Novi zahtjev</p>
+                </div>
+              </a>
+            </Card>
+
+          </div>
+          <ul className="collection" style={{ listStyleType: 'none' }}>
+            {RequestItems}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Requests;
