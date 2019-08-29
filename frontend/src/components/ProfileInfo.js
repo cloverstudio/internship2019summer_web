b73242820ab63e43ade6c98a0c3103caac546876
lsopic <46030196+DevilDeceiver@users.noreply.github.com>
2019-08-29 13:55:24 +0200

    updated profile info

--- frontend/src/components/ProfileInfo.js
+++ frontend/src/components/ProfileInfo.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { FormGroup, FormControl, FormLabel, Button, Image } from 'react-bootstrap';
 import {Redirect} from 'react-router-dom';
+import swal from 'sweetalert';
+import no_content_icon from '../assets/no_content_icon.svg';
+
 
 export default class ProfileInfo extends Component {
 
@@ -8,7 +11,8 @@ export default class ProfileInfo extends Component {
         super(props)
         
         this.state = {
-             profileData: JSON.parse(localStorage.getItem('user'))
+             profileData: JSON.parse(localStorage.getItem('user')),
+             images: no_content_icon,
         }
     }
     
@@ -25,24 +29,70 @@ export default class ProfileInfo extends Component {
         });
     }
 
+    uploadImages = () => {
+        this.setState({
+            // eslint-disable-next-line no-restricted-globals
+            images: URL.createObjectURL(event.target.files[0])
+        })
+    }
+
+    onError = () => {
+        if(this.state.profileData.image === null){
+            return no_content_icon
+        }
+      }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        await fetch('https://intern2019dev.clover.studio/users/myProfile', {
+            method: 'PUT',
+            headers: {
+                'Accept': 'multipart/form-data',
+                'Content-Type': 'multipart/form-data',
+                'token': localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                //firstName: ,
+                //lastName:  ,
+                //oib: ,
+                xhrFields: {
+                    withCredentials: true
+                }
+        }).then(async (res) => {
+            const json = await res.json();
+            console.log(json);
+            swal("Uspješno!", "Vijest uspješno objavljena na portal Moj Grad", "success");
+        })
+        }
+        ).catch(e => {
+            console.log(e);
+            swal("Greška!", "Vijest nije objavljena!", "error");
+        })
+    }
+
 
     render() {
-        console.log(this.state.profileData.oib);
-
-        
-        
-
         return (
-            <div>
-                <form>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+                <form style={{alignSelf:'center'}}>
                     <FormGroup controlId='photo'>
-                        <Image src = {this.state.profileData.Image} roundedCircle />
+                    <input
+                                    type='file'
+                                    onChange={this.uploadImages}
+                                    style={{display:'none'}}
+                                    ref= {fileInput => this.fileInput = fileInput} />
+                        <Image
+                        src = {this.onError()}
+                        roundedCircle
+                        style={{alignSelf:'center', width:'20%'}}
+                         />
                     </FormGroup>
 
                     <FormGroup controlId='name'>
                         <FormControl
-                        value={this.state.profileData.firstName}
+                        value={this.state.profileData.firstName + this.state.profileData.lastName} 
                         required
+                        style={{alignSelf:'center'}}
                         />
                     </FormGroup>
 
@@ -50,6 +100,7 @@ export default class ProfileInfo extends Component {
                         <FormControl
                         required
                         value = {this.state.profileData.oib}
+                        
                         />
                     </FormGroup>
 
@@ -64,7 +115,7 @@ export default class ProfileInfo extends Component {
 
                     <p>U slučaju da zaboravite lozinku, ovdje je možete resetirati</p>
 
-                    <Button>Promijeni podatke</Button>
+                    <Button onClick={this.handleSubmit}>Promijeni podatke</Button>
 
                 </form>
             </div>
