ed24be512972d26efbcc9157cf33ee33d34703bf
chloe0592 <klaudija.ljevar@gmail.com>
2019-09-05 12:38:16 +0200

    request path added on sidebar.js

--- frontend/src/components/requestFolder/NoNewRequests.js
+++ frontend/src/components/requestFolder/NoNewRequests.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import SideBar from './../layout/SideBar';
 import no_content_icon from '../../assets/no_content_icon.svg'; 
 import {  Button} from "react-bootstrap";
 import { Redirect } from 'react-router-dom';
